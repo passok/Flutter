8085360ee80af95efa7324aead7bf25c56f5207b
passok <leopazutti@yahoo.com.br>
2022-12-07 13:33:51 -0300

    Primeiro Projeto - Widgets: row, button

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -18,28 +18,60 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  void decrement(){
+    print('Decrement');
+  }
+
+  void increment(){
+    print('Increment');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.red,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
+        children: [
+          const Text (
             'Pode entrar!',
             style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.w800,
-
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.w800,
             ),
           ),
-          Text(
-              '0',
+          const Text (
+            '0',
             style: TextStyle(
               fontSize: 100,
               color: Colors.white,
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decrement,
+                child: Text(
+                    'Saiu',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                ),
+              ),
+              TextButton(
+                onPressed: increment,
+                child: Text(
+                    'Entrou',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
