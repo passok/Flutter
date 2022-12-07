272064a6c5d5e9308fe28043e1c3c4014de4e1ad
passok <leopazutti@yahoo.com.br>
2022-12-07 13:16:13 -0300

    Primeiro Projeto - Widgets: home, Scaffold, container, column e text

---
+++ contador/android/app/src/main/kotlin/com/projeto/primeiro/contador/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.projeto.primeiro.contador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ contador/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            'Pode entrar!',
+            style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.w800,
+
+            ),
+          ),
+          Text(
+              '0',
+            style: TextStyle(
+              fontSize: 100,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
